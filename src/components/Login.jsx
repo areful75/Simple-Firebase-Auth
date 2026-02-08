6928e14b66dc5371ed6c186d8cb3e7028bc2ce2d
areful75 <mrarefulomi@gmail.com>
2026-02-08 15:23:58 +0600

    added github auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../Firebase.init';
 
@@ -6,6 +6,7 @@ const Login = () => {
 
     const [user,setUser]=useState(null);
     const provider = new GoogleAuthProvider();
+    const providerGithub = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
         console.log('Google Sign-In button clicked');
@@ -20,7 +21,7 @@ const Login = () => {
        //console.log(user);
     }
     const handleSignOut = () => {
-        signOut(auth)
+        signOut(auth,provider)
         .then(()=>{
             console.log('User signed out successfully');
                 setUser(null);
@@ -29,6 +30,26 @@ const Login = () => {
             console.log(error);
         })
     }
+
+    const handleSignInGithub = () => {
+        console.log('GitHub Sign-In button clicked');
+        signInWithPopup(auth, providerGithub)
+        .then((result) => {
+          const logInUser=result.user;
+          if(!logInUser.email && logInUser?.providerData?.length ){
+            console.log('No email found for this user');
+            if(logInUser.providerData[0].email){
+                logInUser.email=logInUser.providerData[0].email;
+                setUser(logInUser);
+            }
+          }
+          setUser(logInUser);
+        })
+         .catch(error=>{
+            console.log(error);
+         })
+    }
+    
     return (
         <div>
            <h2>Please LogIn</h2>
@@ -38,7 +59,9 @@ const Login = () => {
            {
             user ?
             <button onClick={handleSignOut}>Sign Out</button>
-            :<button onClick={handleGoogleSignIn}>Sign In with Google</button>
+            :<>
+            <button onClick={handleGoogleSignIn}>Sign In with Google</button>
+            <button onClick={handleSignInGithub}>Sign In With Github</button></>
            }
            {
             user && <div>
@@ -48,6 +71,9 @@ const Login = () => {
               
             </div>
            }
+           
+          
+
         </div>
     );
 };
